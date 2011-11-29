ed5bfbe8181dadf3a68ffb3e1b8d6d3b928d4375
kennykhek <kennykhek@gmail.com>
2011-11-29 13:02:45 -0800

    Correct variable error, remove unnecessary colon, rename function, correct syntax error, change to use of getFullYear()

--- date-dropdown.js
+++ date-dropdown.js
@@ -10,11 +10,11 @@ To use, include this in html
 
 var monthtext = ['January,'February','March','April','May','June','July','August','September','October','November','December'];
 
-function _init_date-dropdown() {
+function _init_datedropdown() {
     var today = new Date();
     var todayDate = today.getDate();
     var todayMonth = today.getMonth();
-    var todayYear = today.getYear();
+    var todayYear = today.getFullYear();
     var $day = $('.dayfield');
     var $month = $('.monthfield');
     var $year = $('.yearfield');
@@ -22,11 +22,11 @@ function _init_date-dropdown() {
     $day.each(function() {
         for (var i=0; i<31; i++) {
             if (i+1 == todayDate) {
-                this.options[i] = New Option(i+1, i+1, true, true);
+                this.options[i] = new Option(i+1, i+1, true, true);
             } else {
                 this.options[i] = new Option(i+1, i+1);
-            };
-        };
+            }
+        }
     });
 
     $month.each(function() {
@@ -35,19 +35,19 @@ function _init_date-dropdown() {
               this.options[m] = new Option(monthtext[m], m+1, true, true);
             } else {
               this.options[m] = new Option(monthtext[m], m+1);
-            };
-        };
+            }
+        }
     });
 
     $year.each(function() {
         var startYear = todayYear-10;
-             for (var y=0; y<20; y++) {
-                 if (startYear == todayYear) {
-                     this.options[m] = new Option(startYear, startYear, true, true);
-                 } else {
-                     this.options[m] = new Option(startYear, startYear);
-                 };
-             startYear+=1;
-        }; 
+        for (var y=0; y<20; y++) {
+            if (startYear == todayYear) {
+                this.options[y] = new Option(startYear, startYear, true, true);
+            } else {
+                this.options[y] = new Option(startYear, startYear);
+            };
+        startYear+=1;
+        } 
     });
-};
+}
